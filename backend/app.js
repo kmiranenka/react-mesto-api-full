a1818ab2790b31451abe8e457d026b09e295152e
Kate Miranenka <kateway97@gmail.com>
2021-02-27 01:43:45 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -36,13 +36,9 @@ const allowedCors = [
 ];
 
 app.use(function(req, res, next) {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 });
 
 app.use(requestLogger);
